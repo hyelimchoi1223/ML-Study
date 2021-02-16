970e63ce582d84098f97423f0e1d39be2b702853
Hyelim <hlchoi1223@gmail.com>
2021-02-16 21:10:11 +0900

    [Add] Matrix와 벡터에 대한 연산 코드 정리

---
+++ Matrix ans Vector/Matrices.py
@@ -0,0 +1,38 @@
+import numpy as np
+# matrix 생성
+# 결과
+# [[1 1]
+#  [1 3]]
+test_m = np.matrix([[1, 1], [1, 3]])
+print(test_m)
+# matrix dimension
+# 결과
+# (2, 2)
+print(test_m.shape)
+
+A = np.matrix([[1, 0], [2, 5], [3, 1]])
+B = np.matrix([[4, 0.5], [2, 5], [0, 1]])
+# 1. Addition Matrix
+# 결과
+# [[ 5.   0.5]
+# [ 4.  10. ]
+# [ 3.   2. ]]
+print(A+B)
+# 2. Subtration Matrix
+# 결과
+# [[-3.  -0.5]
+# [ 0.   0. ]
+# [ 3.   0. ]]
+print(A-B)
+# 3. Schalar Multiplication
+# 결과
+# [[ 3  0]
+# [ 6 15]
+# [ 9  3]]
+print(A*3)
+# 4. Schalar Divition
+# 결과
+# [0.33333333 0.        ]
+# [0.66666667 1.66666667]
+# [1.         0.33333333]]
+print(A/3)
