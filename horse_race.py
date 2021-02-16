c0cb4f952b12b590f92a62e4db40698218638796
Hyelim <hlchoi1223@gmail.com>
2021-02-16 20:43:43 +0900

    [Delete] 불필요한 파일 삭제

--- horse_race.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-
-race = pd.read_csv(
-    "~/Study/Fastcampus/Project/02_numpy_pandas/datas/archive/2014 p.csv", encoding="euc-kr")
-horses = pd.read_csv(
-    "~/Study/Fastcampus/Project/02_numpy_pandas/datas/archive/2014 s.csv", encoding="euc-kr")
